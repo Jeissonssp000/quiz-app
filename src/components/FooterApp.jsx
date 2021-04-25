77400d401edbf35b61e2d88c72201b0c73f5fe5b
Jeisson Salamanca <jeissonssp@gmail.com>
2021-04-25 07:45:27 -0500

    Take the template delete categories and clean the respective code

--- src/components/FooterApp.jsx
+++ src/components/FooterApp.jsx
@@ -1,9 +1,27 @@
+import {
+  Paper,
+  Container,
+  Typography,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { styles} from "../helpers";
 import React from 'react';
 
+const useStyles = makeStyles((theme) => {
+  return styles;
+});
+
 const FooterApp = () => {
-    <div>
-        <h2>This is a footer example</h2>
-    </div>
+
+  const classes = useStyles();
+
+    <Container>
+      <Paper className={classes.paper}>
+        <Typography variant="h1" className={classes.mainTitle}>
+            Hey, let's finish:
+        </Typography>        
+      </Paper>
+    </Container>
 }
 
 export default FooterApp;
--- src/helpers.js
+++ src/helpers.js
@@ -47,12 +47,6 @@ export const styles = {
   },
 };
 
-export const difficulties = [
-  { id: "total_easy_question_count", name: "Easy" },
-  { id: "total_medium_question_count", name: "Medium" },
-  { id: "total_hard_question_count", name: "Hard" },
-];
-
 export const createMarkup = (text) => {
   return { __html: text };
 };
