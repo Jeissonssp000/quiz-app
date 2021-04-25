d5c9e1851235ff2b41fc42e13af2224173be9d97
Jeisson Salamanca <jeissonssp@gmail.com>
2021-04-25 06:33:39 -0500

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import QuizCategories from './components/QuizCategories';
+
+const App = () => {
+  return (
+    <div>
+      <QuizCategories />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FooterApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FooterApp = () => {
+    <div>
+        <h2>This is a footer example</h2>
+    </div>
+}
+
+export default FooterApp;
